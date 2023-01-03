c041612480e5f4804b11c3d88bbf3bdfc29c5707
derdeche <foued.derdeche@gmail.com>
2023-01-03 11:35:57 +0100

    affichage hotel1

---
+++ index.php
@@ -0,0 +1,11 @@
+
+
+<?php
+spl_autoload_register(function ($class_name) {
+
+require_once $class_name . '.php';
+
+});
+
+$hotel1 = new Hotel ('Hilton', '10 route de la Gare', 'Strasbourg','****', 30, 10, 20);
+echo $hotel1;
