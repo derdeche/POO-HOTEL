5bff0845025ae9598ed97040928d6afdd4d7a40b
derdeche <foued.derdeche@gmail.com>
2023-01-03 09:01:18 +0100

    first commit

---
+++ Hotel.php
@@ -0,0 +1,11 @@
+<?php
+
+class Hotel
+{
+    private string $nom;
+    private string $adresse;
+    private string $ville;
+    private string $etoiles;
+    private array $chambre;
+    private array $reservation;
+}
