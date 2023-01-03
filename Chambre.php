e34f6ad0aa8820c892b123d18bb98e891676a66a
derdeche <foued.derdeche@gmail.com>
2023-01-03 16:42:09 +0100

    création,construct,toString

--- Chambre.php
+++ Chambre.php
@@ -9,3 +9,43 @@ class Chambre
     private string $_etat;
 }
 
+public function __construct(Hotel $hotel, int $chambre, int $prix, bool $wifi, string $etat)
+{
+    $this->_hotel = $hotel;
+    $this->_chambre = $chambre;
+    $this->_prix = $prix;
+    $this->_wifi = $wifi;
+    $this->_etat = $etat;
+}
+
+public function getHotel()
+{
+    return $this->_hotel;
+}
+
+public function getChambre()
+{
+    return $this->_chambre;
+
+}
+
+public function getPrix()
+{
+    return $this->_prix;
+}
+
+public function getWifi()
+{
+    return $this->_wifi;
+}
+
+public function getEtat()
+{
+    return $this->_etat;
+
+}
+        /*Affichage info Chambre*/
+public function __toString()
+{
+    return $this->getHotel().$this->getChambre().$this->getPrix().$this->getWifi().$this->getEtat()."<br>";
+}
