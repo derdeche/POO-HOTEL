bc8e250d66dd40b721108036738c72b37b3057fb
derdeche <foued.derdeche@gmail.com>
2023-01-03 10:52:37 +0100

    rajout public function get, tostring

--- Hotel.php
+++ Hotel.php
@@ -6,7 +6,70 @@ class Hotel
     private string $adresse;
     private string $ville;
     private string $etoiles;
+    private int $nbChambre;
     private int $chReservees;
     private int $chDispo;
+}
+
+public function __construct($nom, $adresse, $ville, $etoiles, $nbChambre, $chReservees, $chDispo)
+{
+    $this->nom = $nom;
+    $this->adresse = $adresse;
+    $this->ville = $ville;
+    $this->etoiles = $etoiles;
+    $this->nbChambre = $nbChambre;
+    $this->chReservees = $chReservees;
+    $this->chDispo = $chDispo;
+}
+
+public function getNom()
+{
+    return $this->nom;
+}
+
+
+public function getAdresse()
+{
+    return $this->adresse;
+}
+
+
+public function getVille()
+{
+    return $this->ville;
+}
+
+
+public function getEtoiles()
+{
+    return $this->etoiles;
+}
+
+
+public function getNbChambre()
+{
+    return $this->nbChambre;
+}
+
+
+public function getChReservees()
+{
+    return $this->chaReservees;
+}
+
+
+public function getChDispo()
+{
+    return $this->chDispo;
+}
+
+public function __toString()
+{
+    echo $this->getNom()." ".$this->getEtoiles()." ".$this->getVille()."<br>";
+    $this->getAdress()." ".$this->getVille()."<br>";
+    echo "Nombre de chambres : ".$this->getNbChambre()."<br>";
+    echo "Nombre de chambres réservées : ".$this->getChReservees."<br>";
+    echo "Nombre de chambres dispo : ".$this->getChDispo;
+
 }
 
