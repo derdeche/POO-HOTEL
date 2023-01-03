375f3518e4080f0b102108cfd1ad96355fa55e08
derdeche <foued.derdeche@gmail.com>
2023-01-03 14:49:21 +0100

    rajout function construct, get

--- Client.php
+++ Client.php
@@ -6,3 +6,26 @@ class Client
     private string $prenom;
     private array $reservations;
 }
+
+public function __construct(string $nom, string $prenom, array $reservations)
+{
+    $this->nom = $nom;
+    $this->prenom = $prenom;
+    $this->reservations = [];
+}
+
+public function getNom()
+{
+    return $this->nom;
+}
+
+public function getPrenom()
+{
+    return $this->prenom;
+}
+
+public function addReservations(array $reservation)
+{
+    $this->reservations[] = $reservation;
+}
+
