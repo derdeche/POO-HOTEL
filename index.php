9b5d26f681dcdb7e782872c1d7035853268043e4
derdeche <foued.derdeche@gmail.com>
2023-01-04 10:46:32 +0100

    affichage hotel2

--- index.php
+++ index.php
@@ -10,6 +10,10 @@ require_once $class_name . '.php';
 $hotel1 = new Hotel ('Hilton', '10 route de la Gare', 'Strasbourg','****', 40, 10, 20);
 $hotel2 = new hotel ('Le Régent','61 Rue Dauphine','Paris','***', 30, 10, 20);
 $hotel1->afficherHotel();
+
+echo "********************************"."<br>";
+$hotel2->afficherHotel();
+
 echo "********************************"."<br>";
 
 
