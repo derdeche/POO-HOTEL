62ed5c3df0703644acd2ca41bb56d87e87e171b8
derdeche <foued.derdeche@gmail.com>
2023-01-04 10:45:06 +0100

    rajout new hotel ,hotel2

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ require_once $class_name . '.php';
 
 });
 
-$hotel1 = new Hotel ('Hilton', '10 route de la Gare', 'Strasbourg','****', 30, 10, 20);
+$hotel1 = new Hotel ('Hilton', '10 route de la Gare', 'Strasbourg','****', 40, 10, 20);
+$hotel2 = new hotel ('Le Régent','61 Rue Dauphine','Paris','***', 30, 10, 20);
 $hotel1->afficherHotel();
 echo "********************************"."<br>";
 
