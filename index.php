6a950b785992b33c3e90d7138a156b0da00044ce
derdeche <foued.derdeche@gmail.com>
2023-01-04 11:06:31 +0100

    création client 1 et 2

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ require_once $class_name . '.php';
 
 $hotel1 = new Hotel ('Hilton', '10 route de la Gare', 'Strasbourg','****', 40, 10, 20);
 $hotel2 = new hotel ('Le Régent','61 Rue Dauphine','Paris','***', 30, 10, 20);
+$client1 = new Client('Virgile', 'GIBELLO');
+$client2 = new Client('Micka', 'MURMANN');
 $hotel1->afficherHotel();
 
 echo "********************************"."<br>";
