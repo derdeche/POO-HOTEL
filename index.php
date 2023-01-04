ce0a6a30d1c4efa609a8f80bcea4634b7b43f862
derdeche <foued.derdeche@gmail.com>
2023-01-04 15:42:43 +0100

    rajout chambre1,2 et 3

--- index.php
+++ index.php
@@ -11,9 +11,12 @@ $hotel1 = new Hotel ('Hilton', '10 route de la Gare', 'Strasbourg','****', 40, 1
 $hotel2 = new hotel ('Le Régent','61 Rue Dauphine','Paris','***', 30, 10, 20);
 $client1 = new Client('Virgile', 'GIBELLO');
 $client2 = new Client('Micka', 'MURMANN');
-$reservation1 = new Reservation($client1, '01-01-2021','03-01-2021',17);
-$reservation2 = new Reservation($client2, '04-01-2021','06-01-2021',3);
-$reservation3 = new Reservation($client2, '08-04-2021','10-04-2021',4);
+$chambre1 = new Chambre($hotel1, 17, 120, 'Oui','Disponible');
+$chambre2 = new Chambre($hotel2, 3, 100, 'Oui','Disponible');
+$chambre3 = new Chambre($hotel2, 4, 100, 'Non', 'Réservée');
+$reservation1 = new Reservation($client1, '2021-01-01','2021-01-03', $chambre1, 120);
+//$reservation2 = new Reservation($client, '04-01-2021','06-01-2021', 3, 100);
+//$reservation3 = new Reservation($client, '08-04-2021','10-04-2021', 4, 100);
 $hotel1->afficherHotel();
 
 echo "********************************"."<br>";
