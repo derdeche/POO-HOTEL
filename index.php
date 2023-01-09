c932df40a9669a187ae614b97eea63919c3ab28e
derdeche <foued.derdeche@gmail.com>
2023-01-09 11:25:11 +0100

    modif affichage

--- index.php
+++ index.php
@@ -13,13 +13,13 @@ $hotel2 = new hotel ('Le Régent','61 Rue Dauphine','Paris','***', 30, 10, 20);
 $client1 = new Client('Virgile', 'GIBELLO');
 $client2 = new Client('Micka', 'MURMANN');
 
-$chambre1 = new Chambre($hotel1, 17, 120, 'Oui','Disponible');
-$chambre2 = new Chambre($hotel1, 3, 100, 'Oui','Disponible');
-$chambre3 = new Chambre($hotel2, 4, 100, 'Non', 'Réservée');
+$chambre1 = new Chambre($hotel1, 17, 120, 'Oui','Disponible',3);
+$chambre2 = new Chambre($hotel1, 3, 100, 'Oui','Disponible',2);
+$chambre3 = new Chambre($hotel2, 4, 100, 'Non', 'Réservée',);
 
-$reservation1 = new Reservation($client1, '01-01-2021','03-01-2021', $chambre1, 120);
-$reservation2 = new Reservation($client2, '04-01-2021','06-01-2021', $chambre2, 100);
-$reservation3 = new Reservation($client2, '08-04-2021','10-04-2021', $chambre3, 100);
+$reservation1 = new Reservation($client1, '01-01-2021','03-01-2021', $chambre1);
+$reservation2 = new Reservation($client2, '04-01-2021','06-01-2021', $chambre2);
+$reservation3 = new Reservation($client2, '08-04-2021','10-04-2021', $chambre3);
 
 $hotel1->afficherHotel();
 echo "********************************"."<br>";
@@ -32,9 +32,9 @@ echo "********************************"."<br>";
 echo $chambre3;
 echo "********************************"."<br>";
 
-//echo $reservation1;
-$client1->afficherReservationClient();
+echo $reservation1;
+//$client1->afficherReservationClient();
 //$hotel1->afficherReservationHotel();
-$reservation1->afficherReservationHotel();
-
-
+//$reservation1->afficherReservationHotel();
+//echo $client1;
+//echo $client1;
