3f8661af9a5f7b27d242e4464a6df764b51c1622
derdeche <foued.derdeche@gmail.com>
2023-01-09 11:36:45 +0100

    modif function construct

--- Reservation.php
+++ Reservation.php
@@ -2,22 +2,23 @@
 
 class Reservation
 {
-    private Client $_client;
+    private Client $_client;    
     private   $_entree;
     private   $_sortie;
     private Chambre $_chambre;
-    private int $prix;
+   
 
 
-public function __construct(Client $client, $entree, $sortie, Chambre $chambre, int $prix)
+public function __construct(Client $client, $entree, $sortie, Chambre $chambre)
 {
     $this->_client = $client;
-    //$this->_client->addReservation($this);
+    $this->_client->addReservation($this);
     $this->_entree = new DateTime ($entree);
     $this->_sortie = new dateTime ($sortie);
     $this->_chambre = $chambre;
-    //$this->_chambre->addReservation($this);
-    $this->_prix = $prix;
+    $this->_chambre->getHotel()->addReservation($this);
+    
+    
 }
 
 public function getClient()
@@ -41,14 +42,11 @@ public function getChambre()
         return $this->_chambre;
     }
 
-public function getPrix()
-    {
-        return $this->_prix;
-    }
+
 
 public function __toString()
 {
-    return $this->getClient();
+    return $this->_client->getNom()." ".$this->_client->getPrenom();
 
 }
 
