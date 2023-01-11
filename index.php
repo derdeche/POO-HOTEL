99cdfbd451504cb555c0e3e6955a4fa1e67de7af
derdeche <foued.derdeche@gmail.com>
2023-01-11 09:22:59 +0100

    rajout ch,reservation,link css,tableauHtml,

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>HOTEL-POO</title>
+</head>
+<body>
+
 
 
 <?php
@@ -7,41 +18,125 @@ require_once $class_name . '.php';
 
 });
 
-$hotel1 = new Hotel ('Hilton', '10 route de la Gare', 'Strasbourg','****', 40, 10, 20);
-$hotel2 = new hotel ('Le Régent','61 Rue Dauphine','Paris','***', 30, 10, 20);
+$hotel1 = new Hotel ('Hilton', '10 route de la Gare', 'Strasbourg','****');
+$hotel2 = new hotel ('Le Régent','61 Rue Dauphine','Paris','***');
 
 $client1 = new Client('Virgile', 'GIBELLO');
 $client2 = new Client('Micka', 'MURMANN');
 
-$chambre1 = new Chambre($hotel1, 17, 120, 'Oui','Disponible',3);
-$chambre2 = new Chambre($hotel1, 3, 100, 'Oui','Disponible',2);
-$chambre3 = new Chambre($hotel2, 4, 120, 'Non', 'Réservée',2);
-$chambre4 = new Chambre($hotel2, 5, 110, 'Oui','Disponible',2);
+$chambre1 = new Chambre($hotel1, 17, 120, 'Oui', 'Disponible',3);
+$chambre2 = new Chambre($hotel1, 3, 100, 'Oui', 'Résérvé',2);
+$chambre3 = new Chambre($hotel2, 4, 120, 'Non', 'Disponible',2);
+$chambre4 = new Chambre($hotel2, 5, 110, 'Oui', 'Résérvé',4);
+$chambre5 = new Chambre($hotel1, 23, 130, 'Oui', 'Disponible',3);
+$chambre6 = new Chambre($hotel1, 30, 140, 'Oui', 'Résérvé',1);
+$chambre7 = new Chambre($hotel2, 41, 150, 'Non', 'Disponible',2);
+$chambre8 = new Chambre($hotel2, 15, 160, 'Oui', 'Résérvé',1);
+
 
 $reservation1 = new Reservation($client1, $chambre1, '01-01-2021','03-01-2021');
-$reservation2 = new Reservation($client1,$chambre4, '04-01-2021','06-01-2021');
-$reservation3 = new Reservation($client2, $chambre3,  '08-04-2021','10-04-2021');
+$reservation2 = new Reservation($client2,$chambre2, '04-01-2021','06-01-2021');
+$reservation3 = new Reservation($client1, $chambre3,  '08-04-2021','10-04-2021');
+$reservation4 = new Reservation($client2,$chambre4, '11-04-2021','15-04-2021');
+$reservation5 = new Reservation($client2, $chambre6,  '09-05-2021','16-05-2021');
+$reservation6 = new Reservation($client2,$chambre5, '25-06-2021','26-06-2021');
 
+
+/*AFFICHAGE INFO HOTEL*/
 $hotel1->infoHotel();
 echo "********************************"."<br>";
+
+/*AFFICHAGE INFO HOTEL*/
 $hotel2->infoHotel();
 echo "********************************"."<br>";
+
+/*AFFICHAGE RESERVATION HOTEL*/
+$hotel1->afficherReservationHotel();
+echo "********************************"."<br>";
+
+/*AFFICHAGE RESERVATION CLIENT*/
+echo "Réservations de ". $client1."<br>";
+echo "<span class='rectangle'> 2 Réservations</span><br>";
+echo $hotel1." / ".$chambre1->infoChambre()."du ".$reservation1->getEntree()->format("d-m-y"). " "."au ".$reservation1->getSortie()->format("d-m-y"). "<br>";
+echo $hotel2." / ".$chambre3->infoChambre()."du ".$reservation3->getEntree()->format("d-m-y"). " "."au ".$reservation3->getSortie()->format("d-m-y"). "<br>";
+echo "********************************"."<br>";
+
+echo "Statut des chambres de ". $hotel1;
+/*$tarif = [
+    " 17" =>"120",
+    " 3" =>"100",
+    " 23"=>"130",
+    " 30"=>"140",
+    ]*/
+    
+    
+    echo afficherTableHtml($chambres);
+    function afficherTableHtml($chambres)
+     {
+
+        $result = "<table border=1>
+        
+                <thead>
+                
+                    <tr>
+                    
+                    <th>CHAMBRE</th>
+                    
+                    <th>TARIF</th>
+                    
+                    <th>WIFI</th>
+                    
+                    <th>ETAT</th>
+                    
+                    </tr>
+                    
+                    </thead>
+                    
+          </tbody>" ;
+
+         
+          
+          foreach ($chambres as $chambre){
+              
+              $result .= "<tr>
+              
+              <td>Chambre 17</td>  
+              
+              <td>120</td>
+              
+              <td><a href='https://fr.wikipedia.org/wiki/$capitale' target='_blank'>lien</td>;
+              
+              
+              </tr>";
+              
+              ksort($capitales);
+              
+            }                        
+            
+            
+            $result .= "</tbody></table>";
+            
+            return $result;
+            
+        }
+
+
 //$hotel2->afficherHotel();
 //echo $chambre1;
-//echo "********************************"."<br>";
+//$reservation1->afficherReservation();
 //echo$chambre2;
-//echo "********************************"."<br>";
 //echo $chambre3;
 //echo "********************************"."<br>";
 //echo $reservation1;
 //echo "********************************"."<br>";
-//$client1->afficherReservationsClient();
 //echo "********************************"."<br>";
 //$hotel1->afficherReservationHotel();
 //echo "********************************"."<br>";
-//$reservation1->afficherReservationHotel();
 //echo $client1;
 //echo $client1;
-//$reservation1->afficherReservation();
 //echo "********************************"."<br>";
 //$reservation1->compteurReservation();
+
+?>
+</body>
+</html>
